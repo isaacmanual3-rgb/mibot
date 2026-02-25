8ccbc00f100819061b16b64a70bc2713c35d2606
isaacmanual3-rgb <isaacmanual3@gmail.com>
2026-02-25 10:56:12 -0400

    Change app port to use environment variable

--- app.py
+++ app.py
@@ -2876,4 +2876,5 @@ _threading.Thread(target=_delayed_webhook_setup, daemon=True).start()
 # ============================================
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=True, host='0.0.0.0', port=port)
